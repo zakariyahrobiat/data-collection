c2e6a11a40ff4e6d33e1dc75f56f7427efdba050
robiat <zakariyahrobiat2018@gmail.com>
2023-03-03 17:04:32 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   chunkSizeWarningLimit: 1600,
-  // },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   // base: "/tweak/",
 });
